ec636dac79a3804fbcf4e83f4ae1632f57d41f68
soorajcfl <soorajcmpm@gmail.com>
2022-12-31 14:30:21 +0530

    Added: Custom hook for fetching time from timeapi.io

---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const API_BASE_URL =
+  'https://www.timeapi.io/api/Time/current/zone?timeZone=';
---
+++ src/hooks/useTimeZoneApi.js
@@ -0,0 +1,32 @@
+import {useState} from 'react';
+import {Alert} from 'react-native';
+
+import {API_BASE_URL} from '../constants';
+
+export const useTimeZoneApi = () => {
+  const [loading, setLoading] = useState(false);
+  const [timeZoneInfo, setTimeZoneInfo] = useState(null);
+
+  const getTimeZoneInfo = timeZone => {
+    setLoading(true);
+    const url = API_BASE_URL + timeZone;
+    fetch(url)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        setLoading(false);
+        setTimeZoneInfo(data);
+      })
+      .catch(error => {
+        setLoading(false);
+        Alert.alert(
+          'Message',
+          'Unable to load the timezone details of ' + timeZone,
+        );
+        console.log('Error while loading time zone details', error);
+      });
+  };
+
+  return [getTimeZoneInfo, loading, timeZoneInfo];
+};
